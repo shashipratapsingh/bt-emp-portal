9b5fa66a11e7c0b64c06a94b4529fea49b62a7f0
shashipratapsingh <itpro8577@gmail.com>
2026-08-06 13:33:15 +0530

    here working on the admin timesheet and working properly

--- src/main/java/EmployeeManagementSystem/entity/RegisterEmployee.java
+++ src/main/java/EmployeeManagementSystem/entity/RegisterEmployee.java
@@ -23,4 +23,5 @@ public class RegisterEmployee {
     private String designation;
 
 
+
 }
--- src/main/java/EmployeeManagementSystem/service/TimesheetService.java
+++ src/main/java/EmployeeManagementSystem/service/TimesheetService.java
@@ -1,14 +1,73 @@
+//package EmployeeManagementSystem.service;
+//
+//import EmployeeManagementSystem.entity.Timesheet;
+//
+//import java.util.List;
+//
+//public interface TimesheetService {
+//     Timesheet saveTimesheet(Timesheet timesheet);
+//     List<Timesheet> getTimesheetsByEmployee(String employeeId);
+//     List<Timesheet> getAllTimesheet();
+//     void updateTimesheetStatus(Long id,String action);
+//
+//     Timesheet getById(Long id);
+//     List<Timesheet> getTimesheetsByEmployeeId(String employeeId);
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 package EmployeeManagementSystem.service;
 
+import EmployeeManagementSystem.dto.EmployeeTimesheetDTO;
 import EmployeeManagementSystem.entity.Timesheet;
 
 import java.util.List;
 
 public interface TimesheetService {
-     Timesheet saveTimesheet(Timesheet timesheet);
-     List<Timesheet> getTimesheetsByEmployee(String employeeId);
-     List<Timesheet> getAllTimesheet();
-     void updateTimesheetStatus(Long id,String action);
 
+     /**
+      * Save Employee Timesheet
+      */
+     Timesheet saveTimesheet(Timesheet timesheet);
+
+     /**
+      * Get Timesheets of Employee
+      */
+     List<Timesheet> getTimesheetsByEmployee(String employeeId);
+
+     /**
+      * Get All Timesheets
+      */
+     List<Timesheet> getAllTimesheet();
+
+     /**
+      * Get Unique Employees (One Row Per Employee)
+      */
+     List<EmployeeTimesheetDTO> getAllEmployees();
+
+     /**
+      * Approve / Reject Timesheet
+      */
+     void updateTimesheetStatus(Long id, String action);
+
+     /**
+      * Get Timesheet By Id
+      */
      Timesheet getById(Long id);
+
+     /**
+      * Get All Timesheets of Selected Employee
+      */
+     List<Timesheet> getTimesheetsByEmployeeId(String employeeId);
 }
