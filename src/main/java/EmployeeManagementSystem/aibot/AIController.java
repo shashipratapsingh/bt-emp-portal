207e632aa6a5c6779a549c706d7ed1beb9a969fb
shashipratapsingh <itpro8577@gmail.com>
2026-08-07 18:08:11 +0530

    updated the ai chatbot

--- src/main/java/EmployeeManagementSystem/aibot/AIController.java
+++ src/main/java/EmployeeManagementSystem/aibot/AIController.java
@@ -1,4 +1,4 @@
-package EmployeeManagementSystem.Aibot;
+package EmployeeManagementSystem.aibot;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
